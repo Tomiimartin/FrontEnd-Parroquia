392ddf825e249ab66b11b1b78acbaee54531982a
Tomas Martin <martintomas898@gmail.com>
2025-03-07 19:05:15 -0300

    Ajustar prebuild.js con variables de entorno

---
+++ rebuild.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const files = ['index.html', 'calendario.html', 'comunidad.html', 'new.html', 'noticias.html'];
+
+files.forEach(file => {
+  let content = fs.readFileSync(file, 'utf8');
+  content = content.replace('${BACKEND_URL}', JSON.stringify(process.env.BACKEND_URL));
+  content = content.replace('${STRAPI_URL}', JSON.stringify(process.env.STRAPI_URL));
+  fs.writeFileSync(file, content);
+});
