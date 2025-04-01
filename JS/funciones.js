9ae739b5be56204a0dae3ac4bfa814367b2d297f
Tomas Martin <martintomas898@gmail.com>
2025-03-31 23:24:51 -0300

    funcion para limpiar imganes de new-index, cambios en paddings de carts-horarios

--- JS/funciones.js
+++ JS/funciones.js
@@ -245,7 +245,7 @@ function crearNew(data) {
 
     const informacionNew = document.createElement('p');
     informacionNew.className = 'informacion-new';
-    informacionNew.textContent = marked.parse(informacion);
+    informacionNew.innerHTML = marked.parse(informacion);
 
     const button = document.createElement("a");
     button.classList.add("btn","VerMasN","btn-principal");
